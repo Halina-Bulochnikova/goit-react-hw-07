feacd2149a07d11122003427984b261766ef71fa
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-05-01 23:30:52 +0200

    add js file, jsx file

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,21 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.default.baseURL = "https://6813df98225ff1af16276298.mockapi.io/";
+
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
+  const response = await axios.get("/contacts");
+  return response.data;
+});
+
+export const addContacts = createAsyncThunk("contacts/add", async () => {
+  const response = await axios.post("/contacts");
+  return response.data;
+});
+export const deleteContacts = createAsyncThunk("contacts/delete", async (contact) => {
+  const response = await axios.delete("/contacts/${id}");
+  return response.id;
+});
+
 
 const initialState = {
   items: [],
+  isLoading: false,
+  error: null,
 };
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState,
-  reducers: {
-    addContact: (state, action) => {
-      state.items.push(action.payload);
-    },
-    deleteContact: (state, action) => {
-      state.items = state.items.filter((contact) => contact.id !== action.payload);
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(addContacts.fulfilled, (state, action) => {
+        state.items = state.items.filter((item) => item.id !== action.payload);
+      });
   },
-});
-
-export const { addContact, deleteContact } = contactsSlice.actions;
-export default contactsSlice.reducer;
+  });
+export default contactsSlice;
