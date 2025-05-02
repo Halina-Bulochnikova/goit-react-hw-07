b8f8085c0d44c1ef4e9dd119f43a997a54a0443e
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-05-02 22:22:15 +0200

    add css file, add jsx file, js files

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -28,8 +28,8 @@ const contactsSlice = createSlice({
       })
 
       .addCase(addContact.fulfilled, (state, action) => {
-        state.items = state.items.filter((item) => item.id !== action.payload);
-      })
+        state.items.push(action.payload);
+})
 
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.items = state.items.filter((item) => item.id !== action.payload);
@@ -44,7 +44,7 @@ export const selectError = (state) => state.contacts.error;
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
   (contacts, filter) => {
-    return contacts.filter((contact) =>
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   }
