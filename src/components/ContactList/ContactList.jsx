bbe9373e6765829c0a25488472ba7def9a1761bd
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-05-03 21:02:43 +0200

    add jsx file, add js file

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,19 +3,13 @@ import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
 import { useSelector } from "react-redux";
 import { selectFilteredContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
 
 function ContactList() {
   const contacts = useSelector(selectFilteredContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
 
   return (
     <ul className={css.contaktList}>
-      {filteredContacts.map((contact) => (
+      {contacts.map((contact) => (
         <Contact
           key={contact.id}
           id={contact.id}
